1885cbb9d20c6d299bf6d70feeaa2649570f6a08
Mac Gibbons <macgibbons@Macs-MacBook-Pro.local>
2021-07-26 16:46:40 -0500

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non, aliquid et debitis consectetur possimus ducimus dolorum quaerat nam animi porro veritatis aut eum ipsa. Iusto eum illum deleniti atque?
+        </p>
+    </article>
+    <article>
+    <h1><a href="/posts/my-second-post">second Post</a></h1>        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non, aliquid et debitis consectetur possimus ducimus dolorum quaerat nam animi porro veritatis aut eum ipsa. Iusto eum illum deleniti atque?
+        </p>
+    </article>
+    <article> 
+        <h1><a href="/posts/my-third-post">third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non, aliquid et debitis consectetur possimus ducimus dolorum quaerat nam animi porro veritatis aut eum ipsa. Iusto eum illum deleniti atque?
+        </p>
+    </article>
+</body>
+</html>
